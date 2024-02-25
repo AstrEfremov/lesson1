a5734f8c50c06f6857063b3b19067c52aaf3d435
Pavel Efremov <astr.efremov@gmail.com>
2024-02-25 12:02:51 +0400

    first commit MyArrayList implementation with tests

--- hw123_description/lesson 2/lesson-2/src/main/java/ru/aston/trainee/lesson2/Lesson2Application.java
+++ hw123_description/lesson 2/lesson-2/src/main/java/ru/aston/trainee/lesson2/Lesson2Application.java
@@ -2,6 +2,9 @@ package ru.aston.trainee.lesson2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.aston.trainee.lesson2.myAraryList.MyArraylist;
+
+import java.util.*;
 
 @SpringBootApplication
 public class Lesson2Application {
@@ -9,5 +12,4 @@ public class Lesson2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Lesson2Application.class, args);
 	}
-
 }
--- hw123_description/lesson 2/lesson-2/src/test/java/ru/aston/trainee/lesson2/Lesson2ApplicationTests.java
+++ hw123_description/lesson 2/lesson-2/src/test/java/ru/aston/trainee/lesson2/Lesson2ApplicationTests.java
@@ -1,13 +1,52 @@
 package ru.aston.trainee.lesson2;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.aston.trainee.lesson2.myAraryList.MyArraylist;
+
+import java.util.Comparator;
 
 @SpringBootTest
 class Lesson2ApplicationTests {
+	MyArraylist<Integer> str = new MyArraylist<>();
+	@BeforeEach
+			void makeAMyArraylist(){
+		str.add(1);
+		str.add(65);
+		str.add(3);
+		str.add(4);
+	}
+	@Test
+	void getAnElement() {
+		System.out.println(str.get(1));
+	}
 
 	@Test
-	void contextLoads() {
+	void getSizeOfArray(){
+		System.out.println(str.size());
+	}
+
+	@Test
+	void removeAnElement(){
+		System.out.println(str.size());
+		str.remove(1);
+		System.out.println(str.size());
+	}
+	@Test
+	void testIsEmpty() {
+		System.out.println(str.size());
+		System.out.println(str.isEmpty());
+	}
+	@Test
+	void testIsClear(){
+		str.clear();
+		System.out.println(str.size());
+	}
+	@Test
+	void sortingArray(){
+		str.sort(Comparator.naturalOrder());
+		System.out.println(str.get(str.size()-1));
 	}
 
 }
